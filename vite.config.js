7e521fbbc5d5a85f4d136c56617ed1f81ccbbb64
Ramkumar K <slashnot84@gmail.com>
2021-06-14 03:14:36 +0530

    Added theme folder config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import yaml from '@rollup/plugin-yaml';
 import copy from 'rollup-plugin-copy';
-import { baseUrl, themeName } from './wp-theme/wp.config.json';
+import config from './wp-theme/wp.config.json';
+
+const {
+  baseUrl='',
+  themeName = 'React-Vite-Wordpress',
+  themeFolder = 'theme-dist'
+} = config;
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
@@ -14,22 +20,19 @@ export default defineConfig(() => {
       // COPY
       copy({
         targets: [
-          { src: 'wp-theme/**', dest: themeName },
+          { src: 'wp-theme/**', dest: `${themeFolder}/${themeName}` },
           { src: ['wp-theme/.*', '!WebKitPoint.config.json'], dest: themeName },
           {
-            src: 'dist/index.html', dest: themeName, rename: 'index.php',
+            src: 'dist/index.html', dest: `${themeFolder}/${themeName}`, rename: 'index.php',
             transform: (contents) => {
               return contents.toString().replaceAll('/assets/', '<?php echo parse_url(get_template_directory_uri(), PHP_URL_PATH) ?>/assets/')
             }
           },
-          { src: 'dist/assets', dest: themeName },
-          { src: 'dist/**/index.*.js', dest: `${themeName}/assets`, transform: (contents => contents.toString().replaceAll('"img",{src:"/assets', `"img",{src:"${baseUrl}/${themeName}/assets/`)) }
+          { src: 'dist/assets', dest: `${themeFolder}/${themeName}` },
+          { src: 'dist/**/index.*.js', dest: `${themeFolder}/${themeName}/assets`, transform: (contents => contents.toString().replaceAll('"img",{src:"/assets', `"img",{src:"${baseUrl}/${themeName}/assets/`)) }
         ],
         hook: 'writeBundle'
       }),
-
-      // TRANSFORM
-
     ]
   }
 })
